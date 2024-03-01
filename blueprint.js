c5523f32380cfff6ce7a11b52074af952d24fbbc
littlecm <clittle.norml@gmail.com>
2024-03-01 11:54:36 -0500

    Add files via upload

---
+++ blueprint.js
@@ -0,0 +1,46 @@
+const blueprint = {
+  "name": "Leads",
+  "slug": "leads",
+  "fields": [
+    {
+      "key": "Customer",
+      "label": "Customer",
+      "type": "string"
+    },
+    {
+      "key": "Customer Email",
+      "label": "Customer Email",
+      "type": "string"
+    },
+    {
+      "key": "Store",
+      "label": "Store",
+      "type": "string"
+    },
+    {
+      "key": "Deal Date Created",
+      "label": "Deal Date Created",
+      "type": "date"
+    },
+    {
+      "key": "Customer First Name",
+      "label": "Customer First Name",
+      "type": "string"
+    },
+    {
+      "key": "Customer Last Name",
+      "label": "Customer Last Name",
+      "type": "string"
+    },
+    {
+      "key": "Source Type",
+      "label": "Source Type",
+      "type": "string"
+    },
+    {
+      "key": "Source Description",
+      "label": "Source Description",
+      "type": "string"
+    }
+  ]
+}
